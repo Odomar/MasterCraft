56fbf4f8e95396c74b46fe0150d5f24eb1f1e4ee
odomar <severingosset@orange.fr>
2020-02-12 17:01:58 +0100

    initial commit

---
+++ glimac/CMakeFiles/glimac.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/src/BlockTypes.cpp" "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/CMakeFiles/glimac.dir/src/BlockTypes.cpp.o"
+  "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/src/Chunk.cpp" "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/CMakeFiles/glimac.dir/src/Chunk.cpp.o"
+  "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/src/Color.cpp" "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/CMakeFiles/glimac.dir/src/Color.cpp.o"
+  "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/src/Cone.cpp" "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/CMakeFiles/glimac.dir/src/Cone.cpp.o"
+  "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/src/Cube.cpp" "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/CMakeFiles/glimac.dir/src/Cube.cpp.o"
+  "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/src/Cube2.cpp" "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/CMakeFiles/glimac.dir/src/Cube2.cpp.o"
+  "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/src/FreeflyCamera.cpp" "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/CMakeFiles/glimac.dir/src/FreeflyCamera.cpp.o"
+  "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/src/Geometry.cpp" "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/CMakeFiles/glimac.dir/src/Geometry.cpp.o"
+  "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/src/Image.cpp" "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/CMakeFiles/glimac.dir/src/Image.cpp.o"
+  "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/src/MasterCraftCamera.cpp" "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/CMakeFiles/glimac.dir/src/MasterCraftCamera.cpp.o"
+  "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/src/Program.cpp" "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/CMakeFiles/glimac.dir/src/Program.cpp.o"
+  "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/src/SDLWindowManager.cpp" "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/CMakeFiles/glimac.dir/src/SDLWindowManager.cpp.o"
+  "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/src/Shader.cpp" "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/CMakeFiles/glimac.dir/src/Shader.cpp.o"
+  "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/src/Sphere.cpp" "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/CMakeFiles/glimac.dir/src/Sphere.cpp.o"
+  "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/src/SuperChunk.cpp" "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/CMakeFiles/glimac.dir/src/SuperChunk.cpp.o"
+  "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/src/TexturesAtlas.cpp" "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/CMakeFiles/glimac.dir/src/TexturesAtlas.cpp.o"
+  "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/src/TrackballCamera.cpp" "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/CMakeFiles/glimac.dir/src/TrackballCamera.cpp.o"
+  "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/src/World.cpp" "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/CMakeFiles/glimac.dir/src/World.cpp.o"
+  "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/src/tiny_obj_loader.cpp" "/home/odomar/Documents/M2/S3/OpenGL/Projet/Mastercraft/glimac/CMakeFiles/glimac.dir/src/tiny_obj_loader.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/SDL"
+  "glimac/include"
+  "third-party/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ glimac/CMakeFiles/glimac.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/glimac.dir/src/BlockTypes.cpp.o"
+  "CMakeFiles/glimac.dir/src/Chunk.cpp.o"
+  "CMakeFiles/glimac.dir/src/Color.cpp.o"
+  "CMakeFiles/glimac.dir/src/Cone.cpp.o"
+  "CMakeFiles/glimac.dir/src/Cube.cpp.o"
+  "CMakeFiles/glimac.dir/src/Cube2.cpp.o"
+  "CMakeFiles/glimac.dir/src/FreeflyCamera.cpp.o"
+  "CMakeFiles/glimac.dir/src/Geometry.cpp.o"
+  "CMakeFiles/glimac.dir/src/Image.cpp.o"
+  "CMakeFiles/glimac.dir/src/MasterCraftCamera.cpp.o"
+  "CMakeFiles/glimac.dir/src/Program.cpp.o"
+  "CMakeFiles/glimac.dir/src/SDLWindowManager.cpp.o"
+  "CMakeFiles/glimac.dir/src/Shader.cpp.o"
+  "CMakeFiles/glimac.dir/src/Sphere.cpp.o"
+  "CMakeFiles/glimac.dir/src/SuperChunk.cpp.o"
+  "CMakeFiles/glimac.dir/src/TexturesAtlas.cpp.o"
+  "CMakeFiles/glimac.dir/src/TrackballCamera.cpp.o"
+  "CMakeFiles/glimac.dir/src/World.cpp.o"
+  "CMakeFiles/glimac.dir/src/tiny_obj_loader.cpp.o"
+  "libglimac.pdb"
+  "libglimac.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/glimac.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
